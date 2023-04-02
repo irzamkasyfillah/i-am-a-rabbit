5ff4ae4d5797c4a8a74aadb836c4ad34a5ad41ff
muhammad.irzam <muhammad.irzam@usenobi.com>
2023-04-03 04:35:10 +0800

    init

--- main.go
+++ main.go
@@ -1,17 +1,9 @@
 package main
 
 import (
-	"context"
-	"encoding/json"
 	"irzam/rabbitmq/rabbitmq"
 )
 
 func main() {
-	body := map[string]interface{}{
-		"message": "Hello World",
-	}
-	bodyJson, _ := json.Marshal(body)
-	rabbitmq.HandlePub(context.Background(), "", "hello", string(bodyJson))
-	// rabbitmq.HandlePub(context.Background(), "", "hell", "Hell dian")
 	rabbitmq.HandleWorker()
 }
